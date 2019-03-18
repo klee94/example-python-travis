70734474b5dcb6f0d619eb676387fc3d75c54cee
klee94 <35122930+klee94@users.noreply.github.com>
2019-03-18 17:34:48 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup, find_packages
 
+package_name = "example-python"
+package_version = "1.0.0"
+
 setup(
-    name = 'python-with-setup-file-example',
+    name=package_name,
+    version=package_version,
+    author="some author",
     install_requires = [
         'django==1.7.1',
         'PyJWT==0.4.2',
@@ -10,5 +15,5 @@ setup(
         'feedparser==5.1.1',
         'pycrypto==2.4',
         'raven==1.9.4'
-    ]
+    ],
 )
