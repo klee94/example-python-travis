3ffd6cc56b2d2a9a59cbb397648abe3d8e152aeb
klee94 <35122930+klee94@users.noreply.github.com>
2019-03-18 17:24:47 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 
-package_name = "example-python"
-package_version = "1.0.0"
-
 setup(
-    name=package_name,
-    version=package_version,
-    author="some author",
-    install_requires=["requests", "numpy", "Django==1.9.6"],
+    name = 'python-with-setup-file-example',
+    install_requires = [
+        'django==1.7.1',
+        'PyJWT==0.4.2',
+        'rsa==3.4',
+        'requests==2.2.1',
+        'feedparser==5.1.1',
+        'pycrypto==2.4',
+        'raven==1.9.4'
+    ]
 )
