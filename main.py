62320a2a6b4b5113fbe8ecbf1a7155ee2cfb5e54
klee94 <35122930+klee94@users.noreply.github.com>
2019-03-18 17:38:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+import hashlib
+
 from jwt import algorithms
-from django.utils import formats
-from rsa import cli
-from requests import sessions
+
+def inner_func():
+    algorithms.HMACAlgorithm(hashlib.sha256).prepare_key('')
 
 if __name__ == '__main__':
-    formats.get_format()
-    algorithms.HMACAlgorithm.prepare_key()
-    cli.VerifyOperation.perform_operation()
-    sessions.SessionRedirectMixin.resolve_redirects()
+    inner_func()
